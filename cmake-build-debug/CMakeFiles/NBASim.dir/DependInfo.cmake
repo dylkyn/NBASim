c648c98ba62f635d1972259390916e4a858fafbf
Christopher Burgett <christopher.burgett@gmail.com>
2021-12-03 16:06:05 -0500

    Adding the Single possession simulation idea.

--- cmake-build-debug/CMakeFiles/NBASim.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/NBASim.dir/DependInfo.cmake
@@ -4,26 +4,11 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/kynoc/OneDrive/Documents/GT/Classes/ECE 4122/NBASim/main.cpp" "C:/Users/kynoc/OneDrive/Documents/GT/Classes/ECE 4122/NBASim/cmake-build-debug/CMakeFiles/NBASim.dir/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "SFML_STATIC"
-  )
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../SFML/include"
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/cjburgett/Documents/ECE_4122/Final_project_real/main.cpp" "CMakeFiles/NBASim.dir/main.cpp.o" "gcc" "CMakeFiles/NBASim.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
