cec945f02708ac74cbd6c4d22ab49e4f87ed4b45
Dylan Kynoch <kynochd@gmail.com>
2021-12-03 14:59:35 -0500

    Start

---
+++ cmake-build-debug/CMakeFiles/NBASim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NBASim.dir/main.cpp.obj"
+  "NBASim.exe"
+  "NBASim.exe.manifest"
+  "NBASim.pdb"
+  "libNBASim.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NBASim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NBASim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NBASim.
+# This may be replaced when dependencies are built.
