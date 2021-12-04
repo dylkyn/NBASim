f6ab3d58d85ef645f3f2ba26b6897d6a9ae9ceca
Justin Lau <55557723+jlau0170@users.noreply.github.com>
2021-12-04 14:44:02 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -17,6 +17,14 @@ private:
     int threePointShot;
     int twoPointShot;
 
+    //setting unique name
+    string name;
+    int pointGuardRange = 30;
+    int shootingGuardRange = 30;
+    int smallForwardRange = 30;
+    int powerForwardRange = 30;
+    int centerRange = 30;
+
     //player stats
     int numMissedTwo;
     int numMissedThree;
@@ -56,6 +64,168 @@ public:
     int getNumAssist();
     int getNumRebounds();
     int getNumSteals();
-    int getNumBlocks();
+    int getNumBlocks()
+
+    //string array of names
+    string pointGuardNames[30] = {
+        "Trae Young",
+        "Jose Alvarado",
+        "Stephen Curry",
+        "LaMelo Ball",
+        "Luka Doncic",
+        "Damian Lillard",
+        "Kyrie Irving",
+        "Jeremy Lin",
+        "Chris Paul",
+        "Ja Morant",
+        "Lonzo Ball",
+        "Rajon Rondo",
+        "Derrick Rose",
+        "Ricky Rubio",
+        "D'Angelo Russell",
+        "Collin Sexton",
+        "Ben Simmons",
+        "Patrick Beverley",
+        "Cade Cunningham",
+        "De'Aaron Fox",
+        "Shai Gilgeous-Alexander",
+        "Jrue Holiday",
+        "Kyle Lowry",
+        "Jamal Murray",
+        "Dennis Schroder",
+        "John Wall",
+        "Russell Westbrook",
+        "Markelle Fultz",
+        "Avery Bradley",
+        "Jordan Clarkson"
+    }
+    string shootingGuardNames[30] = {
+        "Bradley Beal",
+        "James Harden",
+        "Devin Booker",
+        "Jaylen Brown",
+        "RJ Barrett",
+        "Alex Caruso",
+        "Donte DiVincenzo",
+        "Evan Fournier",
+        "Jalen Green",
+        "Buddy Hield",
+        "Talen Horton-Tucker",
+        "Kevin Huerter",
+        "Joe Ingles",
+        "Victor Oladipo",
+        "Immanuel Quickley",
+        "Josh Richardson",
+        "Rodney Hood",
+        "Gary Harris",
+        "Josh Giddey",
+        "Donovan Mitchell",
+        "Austin Rivers",
+        "Terry Rozier",
+        "Landry Shamet",
+        "Klay Thompson",
+        "Lou Williams",
+        "Luke Kennard",
+        "Zach Lavine",
+        "Caris LeVert",
+        "Jalen Suggs",
+        "Gary Trent Jr."
+    }
+    string smallForwardNames[30] = {
+        "Lebron James",
+        "Paul George",
+        "Khris Middleton",
+        "Jimmy Butler",
+        "Demar Derozan",
+        "OG Anunoby",
+        "Trevor Ariza",
+        "Anthony Edwards",
+        "Rudy Gay",
+        "Gordon Hayward",
+        "Andre Iguodala",
+        "Brandon Ingram",
+        "Kawhi Leonard",
+        "Josh Okogie",
+        "Eric Paschall",
+        "Isaac Okoro",
+        "Jayson Tatum",
+        "Yuta Watanabe",
+        "Tony Snell",
+        "Norman Powell",
+        "Otto Porter Jr.",
+        "Bruno Fernando",
+        "Danny Green",
+        "Dillon Brooks",
+        "Miles Bridges",
+        "Kent Bazemore",
+        "Bojan Bogdanovic",
+        "Tyler Cook",
+        "Lugentz Dort",
+        "Thanasis Antetokounmpo",
+    }
+    string powerForwardNames[30] = {
+        "Giannis Antetokounmpo",
+        "Blake Griffin",
+        "Carmelo Anthony",
+        "Nicholas Batum",
+        "Anthony Davis",
+        "Kevin Durant",
+        "Jae Crowder",
+        "Robert Covington",
+        "Marvin Bagley III",
+        "Jeff Green",
+        "Rui Hachimura",
+        "Jalen Mcdaniels",
+        "Jaden Mcdaniels",
+        "Kevin Love",
+        "Paul Millsap",
+        "Jabari Parker",
+        "Julius Randle",
+        "P.J. Tucker",
+        "Jared Vanderbilt",
+        "Zion Williamson",
+        "Thaddeus Young",
+        "Kelly Olynyk",
+        "Udonis Haslem",
+        "Tobias Harris",
+        "John Collins",
+        "Darius Bazley",
+        "Nemanja Bjelica",
+        "Draymond Green",
+        "Aaron Gordon",
+        "Isaiah Jackson"
+    }
+    string centerNames[30] = {
+        "Steven Adams",
+        "Bam Adebayo",
+        "Lamarcus Aldridge",
+        "Jarrett Allen",
+        "Deandre Ayton",
+        "Mo Bamba",
+        "Bol Bol",
+        "Moses Brown",
+        "Clint Capela",
+        "Demarcus Cousins",
+        "Cody Zeller",
+        "Nikola Vucevic",
+        "Myles Turner",
+        "Mitchell Robinson",
+        "Dwight Powell",
+        "Jakob Poetl",
+        "Evan Mobley",
+        "JaVale Mcgee",
+        "Al Horford",
+        "Serge Ibaka",
+        "DeAndre Jordan",
+        "Derrick Favors",
+        "Tacko Fall",
+        "Andre Drummond",
+        "Kevon Looney",
+        "Robin Lopez",
+        "Brook Lopez",
+        "Evan Mobley",
+        "Nerlens Noel",
+        "Jusuf Nurkic"
+    }
 };
 #endif //NBASIM_PLAYER_H
