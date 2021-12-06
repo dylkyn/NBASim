53644c9eca5144db084a1ab29888463f4ce2399e
ipoage <72356532+ipoage@users.noreply.github.com>
2021-12-06 14:19:33 -0500

    start SFML

--- Team.cpp
+++ Team.cpp
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <iostream>
 
-Team::Team(std::string name)
+Team::Team(std::string name std::string logo)
 {
     players[0] = Player(1); //point guard
     players[1] = Player(2); //shooting guard
@@ -11,6 +11,7 @@ Team::Team(std::string name)
     players[3] = Player(4); //power forward
     players[4] = Player(5); //small forward
     teamName = name;
+    teamLogo = logo;
     gameScore = 0;
 }
 Team::Team()
@@ -39,4 +40,8 @@ int Team::getGameScore() {
 std::string Team::getName()
 {
     return teamName;
+}
+std::string Team::getLogo()
+{
+    return teamLogo;
 }
--- Team.h
+++ Team.h
@@ -8,13 +8,15 @@ private:
     Player players[5];
     int gameScore;
     std::string teamName;
+    std::string teamLogo;
 public:
     Team();
-    Team(std::string name);
+    Team(std::string name, std::string logo);
     Player getPlayer();
     int getGameScore();
     void resetTeamScore();
     void setGameScore(int score);
     std::string getName();
+    std::string getLogo();
 };
 #endif //NBASIM_TEAM_H
