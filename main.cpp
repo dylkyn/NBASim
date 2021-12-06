0bfdc8cecca468332723deeba2e0f51ddf0cacf6
ipoage <72356532+ipoage@users.noreply.github.com>
2021-12-06 14:44:15 -0500

    start SFML

--- main.cpp
+++ main.cpp
@@ -109,32 +109,157 @@ int main()
 
     Team warriors("Warriors", "graphics/warriors.png");
     Texture textureWarriors;
-    textureThunder.loadFromFile(warriors.getLogo());
-    Sprite spriteThunder;
-    spriteThunder.setTexture(textureThunder);
-    teams.push_back(thunder);
+    textureWarriors.loadFromFile(warriors.getLogo());
+    Sprite spriteWarriors;
+    spriteWarriors.setTexture(textureWarriors);
+    teams.push_back(warriors);
 
     Team rockets("Rockets", "graphics/rockets.png");
+    Texture textureRockets;
+    textureRockets.loadFromFile(rockets.getLogo());
+    Sprite spriteRockets;
+    spriteRockets.setTexture(textureRockets);
+    teams.push_back(rockets);
+
     Team pacers("Pacers", "graphics/pacers.png");
+    Texture texturePacers;
+    texturePacers.loadFromFile(pacers.getLogo());
+    Sprite spritePacers;
+    spritePacers.setTexture(texturePacers);
+    teams.push_back(pacers);
+
     Team lakers("Lakers", "graphics/lakers.png");
+    Texture textureLakers;
+    textureLakers.loadFromFile(lakers.getLogo());
+    Sprite spriteLakers;
+    spriteLakers.setTexture(textureLakers);
+    teams.push_back(lakers);
+
     Team grizzlies("Grizzlies", "graphics/grizzlies.png");
+    Texture textureGrizzlies;
+    textureGrizzlies.loadFromFile(grizzlies.getLogo());
+    Sprite spriteGrizzlies;
+    spriteGrizzlies.setTexture(textureGrizzlies);
+    teams.push_back(grizzlies);
+
     Team bucks("Bucks", "graphics/bucks.png");
+    Texture textureBucks;
+    textureBucks.loadFromFile(bucks.getLogo());
+    Sprite spriteBucks;
+    spriteBucks.setTexture(textureBucks);
+    teams.push_back(bucks);
+
     Team timberwolves("Timberwolves", "graphics/timberwolves.png");
+    Texture textureTimberwolves;
+    textureTimberwolves.loadFromFile(timberwolves.getLogo());
+    Sprite spriteTimberwolves;
+    spriteTimberwolves.setTexture(textureTimberwolves);
+    teams.push_back(timberwolves);
+
     Team pelicans("Pelicans", "graphics/pelicans.png");
+    Texture texturePelicans;
+    texturePelicanss.loadFromFile(pelicans.getLogo());
+    Sprite spritePelicans;
+    spritePelicans.setTexture(texturePelicans);
+    teams.push_back(pelicans);
+
     Team knicks("Knicks", "graphics/knicks.png");
+    Texture textureKnicks;
+    textureKnicks.loadFromFile(knicks.getLogo());
+    Sprite spriteKnicks;
+    spriteKnicks.setTexture(textureKnicks);
+    teams.push_back(knicks);
+
     Team magic("Magic", "graphics/magic.png");
+    Texture textureMagic;
+    textureMagic.loadFromFile(magic.getLogo());
+    Sprite spriteMagic;
+    spriteMagic.setTexture(textureMagic);
+    teams.push_back(magic);
+
     Team the76ers("76ers", "graphics/76ers.png");
+    Texture texture76ers;
+    texture76ers.loadFromFile(the76ers.getLogo());
+    Sprite sprite76ers;
+    sprite76ers.setTexture(texture76ers);
+    teams.push_back(the76ers);
+
     Team suns("Suns", "graphics/suns.png");
+    Texture textureSuns;
+    textureSuns.loadFromFile(suns.getLogo());
+    Sprite spriteSuns;
+    spriteSuns.setTexture(textureSuns);
+    teams.push_back(suns);
+
     Team blazers("Blazers", "graphics/blazers.png");
+    Texture textureBlazers;
+    textureBlazers.loadFromFile(blazers.getLogo());
+    Sprite spriteBlazers;
+    spriteBlazers.setTexture(textureBlazers);
+    teams.push_back(blazers);
+
     Team spurs("Spurs", "graphics/spurs.png");
+    Texture textureSpurs;
+    textureSpurs.loadFromFile(spurs.getLogo());
+    Sprite spriteSpurs;
+    spriteSpurs.setTexture(textureSpurs);
+    teams.push_back(spurs);
+
     Team raptors("Raptors", "graphics/raptors.png");
+    Texture textureRaptors;
+    textureRaptors.loadFromFile(raptors.getLogo());
+    Sprite spriteRaptors;
+    spriteRaptors.setTexture(textureRaptors);
+    teams.push_back(raptors);
+
     Team jazz("Jazz", "graphics/jazz.png");
+    Texture textureJazz;
+    textureJazz.loadFromFile(jazz.getLogo());
+    Sprite spriteJazz;
+    spriteJazz.setTexture(textureJazz);
+    teams.push_back(jazz);
+
     Team heat("Heat", "graphics/heat.png");
+    Texture textureHeat;
+    textureHeat.loadFromFile(heat.getLogo());
+    Sprite spriteHeat;
+    spriteHeat.setTexture(textureHeat);
+    teams.push_back(heat);
+
     Team wizards("Wizards", "graphics/wizards.png");
+    Texture textureWizards;
+    textureWizards.loadFromFile(wizards.getLogo());
+    Sprite spriteWizards;
+    spriteWizards.setTexture(textureWizards);
+    teams.push_back(wizards);
+
     Team clippers("Clippers", "graphics/clippers.png");
+    Texture textureClippers;
+    textureClippers.loadFromFile(clippers.getLogo());
+    Sprite spriteClippers;
+    spriteClippers.setTexture(textureClippers);
+    teams.push_back(clippers);
+
     Team kings("Kings", "graphics/kings.png");
+    Texture textureKings;
+    textureKings.loadFromFile(kings.getLogo());
+    Sprite spriteKings;
+    spriteKings.setTexture(textureKings);
+    teams.push_back(kings);
+
     Team buzz("Buzz", "graphics/buzz.png");
+    Texture textureBuzz;
+    textureBuzz.loadFromFile(buzz.getLogo());
+    Sprite spriteBuzz;
+    spriteBuzz.setTexture(textureBuzz);
+    teams.push_back(buzz);
+
     Team wreck("Ramblin' Wreck", "graphics/wreck.png");
+    Texture textureWreck;
+    textureWreck.loadFromFile(wreck.getLogo());
+    Sprite spriteWreck;
+    spriteWreck.setTexture(textureWreck);
+    teams.push_back(wreck);
 
     Game game1(hawks, thunder);
     Game game2(heat, lakers);
